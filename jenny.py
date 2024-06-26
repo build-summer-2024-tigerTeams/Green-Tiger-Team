ac09a9448949d42b95d12c1e24cd95134bc7a10c
jennyma305 <jennyma305@gmail.com>
2024-06-25 17:23:07 -0700

    update
    update

---
+++ jenny.py
@@ -0,0 +1,10 @@
+for i in range(10):
+    print("hello world")
+
+list = [1, 3, 4, 2, 5, 5]
+target = 2
+index = list.index(target)
+print(f"Found {target} at index: {index}")
+
+
+print('Tiger Team Green')
