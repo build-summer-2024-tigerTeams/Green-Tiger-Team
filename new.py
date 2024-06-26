a2565ea87ae04a3c28fd639549736bd616ea5056
zmahoney5 <131542537+zmahoney5@users.noreply.github.com>
2024-06-25 17:32:38 -0700

    new.py

--- new.py
+++ new.py
@@ -1,8 +1,10 @@
 for i in range(10):
     print("Hello World!")
 
-list = [1,3,4,2,5,5]
-index_of_2 = first.index(2)
-print(index_of_2)
+target=2 
+list =[1,3,4,2,5,5]
+for i in range(len(list)):
+    if target == list[i]:
+        print("Found at index: "+i)
 
 print("Tiger Team Green")
